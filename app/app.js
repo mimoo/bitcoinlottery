e566aaa16c8d77753dcc35484489398c8a3739bb
Eric Khun <eric@buffer.com>
2015-06-14 01:46:29 +0800

    check if user can play or not with asynchrinous function

--- app/app.js
+++ app/app.js
@@ -12,66 +12,86 @@ db.once('open', function (callback) {
   console.log("Connection to mongo successful!");
 });
 
+var MAX_REPLAY = 3;
+
+// TODO : Put this in an other file
+// Visitor model that match the db
+var visitorSchema = mongoose.Schema({
+    ip: { type: String, default: "unknown" },
+    date: { type: Date, default: Date.now },
+});
+var visitorModel = mongoose.model('Visitor', visitorSchema);
 
 // Make assets folder accessible
 app.use("/assets/", express.static(__dirname + '/assets'));
 
 function createKeys() {
   var key = bitcoin.ECKey.makeRandom()
-  return {"private_key" : key.toWIF() , "public_key" : key.pub.getAddress().toString()}
+  return {
+            "private_key" : key.toWIF(),
+            "public_key" : key.pub.getAddress().toString()
+          }
 }
 
-function canPlay() {
-  return true;
-}
-
-function updatePlayer() {
-
-}
-
-// Improvement : make this method with callback to make sure it save in mongo 
-// But now it's called before an API request, so we're almost sure it's saved before
-function saveVisit(req) {
-  var visitorSchema = mongoose.Schema({
-      ip: { type: String, default: req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress },
-      date: { type: Date, default: Date.now },
+// Be careful : this is an asynchronous function
+// so use the callback when we use it :) http://stackoverflow.com/a/6898978
+var canPlay = function(ip, callback){
+  visitorModel.count({ip: ip}, function(err, count)
+  {
+    if (count < MAX_REPLAY)
+      callback(true); // can still play
+    else
+      callback(false);
   });
+};
 
-  var Visitor = mongoose.model('Visitor', visitorSchema);
-  var newVisitor = new Visitor();
+function getIP(req) {
+  return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+}
 
-  newVisitor.save(function (err, visitor) {
+// TODO : make this method with callback to make sure it save in mongo 
+// But now it's called before an API request, so we're almost sure it's saved before
+function saveVisit(ip) {
+
+  var visitor = new visitorModel( {ip : ip});
+  visitor.save(function (err, visitor) {
     if (err) return console.error(err);
   });
 }
 
 app.get('/', function(req, res) {
 
-  if (canPlay() === true)
-    res.sendFile(__dirname + "/index.html");
-  else
-    res.sendFile(__dirname + "/no.html");
+  canPlay(getIP(req), function (possible) {
+
+    if (possible === true)
+      res.sendFile(__dirname + "/index.html");
+    else
+      res.sendFile(__dirname + "/no.html");
+
+  });
+
 
 });
 
 app.get('/play', function(req, res){
 
-  saveVisit(req); 
+  var ip_address = getIP(req);
+  saveVisit(ip_address);
   var keys = createKeys();
   console.log(keys);
 
   request('https://blockchain.info/address/' + keys.public_key +'?format=json', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       body = JSON.parse(body);
-      console.log(body.final_balance);
-      res.json(
-        {
+
+      canPlay(ip_address, function(possible) {
+        res.json({
           "balance": body.final_balance,
           "public_key" : keys.public_key,
           "private_key" : keys.private_key,
-          "can_play_again" : canPlay()
-        }
-      );
+          "can_play_again" : possible
+        });
+      });
     }
     else {
       res.json(
