9e9326a6c2af92a6231583c3655e93a21727a46e
Eric Khun <eric@buffer.com>
2015-06-13 12:31:32 +0800

    add views

--- app/app.js
+++ app/app.js
@@ -3,13 +3,14 @@ var bitcoin = require('bitcoinjs-lib');
 var app = express();
 
 
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + "/index.html"); 
 });
 
 
 app.get('/play', function(req, res){
-  console.log("Paying....");
+  console.log("Palying....");
 });
 
 
