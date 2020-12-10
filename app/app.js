dd255e8e93d704a5fd7067514ecbfc7f87aba1ea
Eric Khun <enregistrements@gmail.com>
2020-12-10 22:24:35 +0000

    add authSource=admin

--- app/app.js
+++ app/app.js
@@ -34,7 +34,7 @@ const mongouser = process.env.MONGO_USERNAME
 console.log(`mongodb://${mongouser}:${mongopwd}@mongo:27017/bitcoinlottery`);
 
 if (MAX_REPLAY > 0) {
-	mongoose.connect(`mongodb://${mongouser}:${mongopwd}@mongo:27017/bitcoinlottery`, { useNewUrlParser: true });
+	mongoose.connect(`mongodb://${mongouser}:${mongopwd}@mongo:27017/bitcoinlottery?authSource=admin`, { useNewUrlParser: true });
 	var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function (callback) {
